9df6e921076546528b8529b2f403a14e42f713ae
tdxa <anndoman3@gmail.com>
2020-05-25 19:49:02 +0200

    binding error

--- app/src/main/java/com/example/kotlinpostapi/photos/PhotoAdapter.kt
+++ app/src/main/java/com/example/kotlinpostapi/photos/PhotoAdapter.kt
@@ -2,21 +2,23 @@ package com.example.kotlinpostapi.photos
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.kotlinpostapi.R
 import com.example.kotlinpostapi.apiObjects.Photo
 import com.example.kotlinpostapi.databinding.PhotoViewBinding
+import kotlinx.android.synthetic.main.photo_view.view.*
 
 class PhotoAdapter(private var photos: List<Photo>) : RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>(){
 
     inner class PhotoViewHolder(private val binding: PhotoViewBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun bind(photo: Photo){
-            binding.photo.url = photo.url
+            binding.photo= photo
             binding.executePendingBindings()
 
-            
-            Glide.with()
+            Glide.with(binding.root)
                 .load(photo.url)
                 .into(binding.photoImageView)
         }
@@ -26,8 +28,6 @@ class PhotoAdapter(private var photos: List<Photo>) : RecyclerView.Adapter<Photo
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = PhotoViewBinding.inflate(layoutInflater)
         return PhotoViewHolder(binding)
-
-
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/kotlinpostapi/views/PhotoList.kt
+++ app/src/main/java/com/example/kotlinpostapi/views/PhotoList.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
@@ -34,6 +35,7 @@ class PhotoList : Fragment() {
         setupRecyclerView()
         observeLiveData()
         getAlbum(args.albumId)
+
         return binding.root
     }
 
