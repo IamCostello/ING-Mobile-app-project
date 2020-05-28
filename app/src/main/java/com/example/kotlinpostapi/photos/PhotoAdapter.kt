77883cd70c5529c8d65854a91012670e1e4158e7
tdxa <anndoman3@gmail.com>
2020-05-28 14:07:00 +0200

    Add photo placeholder

--- app/src/main/java/com/example/kotlinpostapi/photos/PhotoAdapter.kt
+++ app/src/main/java/com/example/kotlinpostapi/photos/PhotoAdapter.kt
@@ -2,22 +2,20 @@ package com.example.kotlinpostapi.photos
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.Glide.with
 import com.example.kotlinpostapi.R
 import com.example.kotlinpostapi.apiObjects.Photo
 import com.example.kotlinpostapi.databinding.PhotoViewBinding
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.photo_view.view.*
 
-class PhotoAdapter(private var photos: List<Photo>) : RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>(){
+class PhotoAdapter(private var photos: List<Photo>) :
+    RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
 
-    inner class PhotoViewHolder(private val binding: PhotoViewBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class PhotoViewHolder(private val binding: PhotoViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(photo: Photo){
-            binding.photo= photo
+        fun bind(photo: Photo) {
+            binding.photo = photo
             binding.executePendingBindings()
 
 //            Glide.with(binding.root)
@@ -26,6 +24,8 @@ class PhotoAdapter(private var photos: List<Photo>) : RecyclerView.Adapter<Photo
             Picasso
                 .get()
                 .load(photo.url)
+                .placeholder(R.drawable.photo_placeholder)
+                .error(R.drawable.photo_error)
                 .into(binding.photoImageView)
         }
     }
@@ -40,9 +40,10 @@ class PhotoAdapter(private var photos: List<Photo>) : RecyclerView.Adapter<Photo
         return photos.size
     }
 
-    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) = holder.bind(photos[position])
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) =
+        holder.bind(photos[position])
 
-    fun updatePhoto(photos: List<Photo>){
+    fun updatePhoto(photos: List<Photo>) {
         this.photos = photos
         notifyDataSetChanged()
     }
